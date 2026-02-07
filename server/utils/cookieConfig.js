f19c0fff91181768092d2fa3953cdc81a0d81257
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-02-08 01:44:03 +0200

    fixing cookie config

--- server/utils/cookieConfig.js
+++ server/utils/cookieConfig.js
@@ -2,5 +2,6 @@ export const cookieConfig = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  domain: ".wahaj.it.com",
   maxAge: 1000 * 60 * 60 * 24 * 30,
 };
