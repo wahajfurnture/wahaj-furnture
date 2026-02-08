95613179d905f23fe20f3de62e3bb520754d2553
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-02-08 02:12:37 +0200

    changing cookie setting

--- server/utils/cookieConfig.js
+++ server/utils/cookieConfig.js
@@ -1,7 +1,10 @@
 export const cookieConfig = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   domain: ".wahaj.it.com",
+  path: "/",
   maxAge: 1000 * 60 * 60 * 24 * 30,
+
+  partitioned: true,
 };
