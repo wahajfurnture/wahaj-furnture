11d8e3b3491a40b78cff092c37380a14cb517852
Omar-Yasser-Frontend <craft14716@gmail.com>
2026-02-08 14:49:32 +0200

    cookie config

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,14 +5,6 @@ const nextConfig = {
   images: {
     remotePatterns: [new URL("https://res.cloudinary.com/digmekbtt/**")],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "https://api.wahaj.it.com/api/v1/:path*",
-      },
-    ];
-  },
 };
 
 export default withNextIntl(nextConfig);
--- server/utils/cookieConfig.js
+++ server/utils/cookieConfig.js
@@ -1,6 +1,8 @@
 export const cookieConfig = {
   httpOnly: true,
   secure: true,
-  sameSite: "strict",
+  sameSite: "lax",
+  domain: "wahaj.it.com",
+  path: "/",
   maxAge: 1000 * 60 * 60 * 24 * 30,
 };
